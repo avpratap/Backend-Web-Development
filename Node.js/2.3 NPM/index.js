c25deeea87fe61583549592e04f667c5a9f130cf
avpratap <vimlapratap25@gmail.com>
2024-08-10 13:09:52 +0530

    first commit

---
+++ Node.js/2.3 NPM/index.js
@@ -0,0 +1,16 @@
+//var generateName = require('sillyname');
+
+import generateName from "sillyname";
+var sillyName = generateName();
+
+  console.log("My name is " + sillyName + ".");
+
+ 
+
+  import superheroes from "superheroes";
+
+var name = randomSuperheroes();
+console.log("I am " + name + "!");
+
+
+
